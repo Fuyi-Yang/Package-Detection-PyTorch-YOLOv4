381df12edefae4ce1e43c28c6711f9747113b897
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2023-01-18 07:44:31 -0800

    Update alert.py

--- tool/alert.py
+++ tool/alert.py
@@ -1,7 +1,12 @@
 import smtplib
 from email.message import EmailMessage
+import pyautogui
+import pywhatkit
+import datetime
+import time
+from instabot import Bot
 
-def email_alert(subject, body, to):
+def email_alert(subject, body, to): # Sends message to Email or phone number
     msg = EmailMessage()
     msg.set_content(body)
     msg['subject'] = subject
@@ -18,5 +23,22 @@ def email_alert(subject, body, to):
     print("email sent")
     server.quit()
 
+def whatsapp_alert(body, to): #Sends message to whatsapp;  requires phone number
+    time = datetime.datetime.now()
+    pywhatkit.sendwhatmsg(to, body, time.hour, time.minute)
+
+def messenger_alert(body, to): #Sends message to Messenger; cursor needed
+    pyautogui.typewrite(body)
+    pyautogui.press('enter')
+
+def instgram_alert(body, to): #Sends message to Instagram; requires username
+    mybot = Bot()
+    mybot.login(username = "", password= "")
+    send_to = mybot.get_user_id_from_username(to)
+    mybot.follow(send_to)
+    mybot.send_message(body, [send_to])
+
 if __name__ == '__main__':
-    email_alert("hello my friend", "hello world", "xxx@email.com")
+    email_alert("Your package has arrived",
+                "Hi, \n\n We have detected that your package has arrived. Please pick it up!!", "xxx@email.com")
+    whatsapp_alert("Hi, \n\n We have detected that your package has arrived. Please pick it up!!", "+1 1111111111")
